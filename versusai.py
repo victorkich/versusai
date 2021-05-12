2684227bba603ae722d5bca79a4d34fdbe3d8ca7
victorkich <victorkich@yahoo.com.br>
2021-05-12 18:32:15 -0300

    Creating the author page.

--- versusai.py
+++ versusai.py
@@ -10,6 +10,34 @@ def main():
 def victor():
     return render_template('victor.html')
 
+@app.route("/dranaju")
+def dranaju():
+    return render_template('dranaju.html')
+
+@app.route("/alisson")
+def alisson():
+    return render_template('alisson.html')
+
+@app.route("/ricardo")
+def ricardo():
+    return render_template('ricardo.html')
+
+@app.route("/eduardo")
+def eduardo():
+    return render_template('eduardo.html')
+
+@app.route("/nicolas")
+def nicolas():
+    return render_template('nicolas.html')
+
+@app.route("/jair")
+def jair():
+    return render_template('jair.html')
+
+@app.route("/marcelo")
+def marcelo():
+    return render_template('marcelo.html')
+
 @app.route("/home")
 def home():
     return render_template('index.html')
